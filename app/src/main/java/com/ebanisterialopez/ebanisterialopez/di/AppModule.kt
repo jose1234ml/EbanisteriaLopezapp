ba9f222254d6adf535a130ae714a803202e996d6
malvin <Malvinjose16@gmail.com>
2025-11-19 14:34:46 -0400

    Crea AppModule

---
+++ app/src/main/java/com/ebanisterialopez/ebanisterialopez/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.ebanisterialopez.ebanisterialopez.di
+
+
+import com.ebanisterialopez.ebanisterialopez.data.api.AuthApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthApiService(retrofit: Retrofit): AuthApiService {
+        return retrofit.create(AuthApiService::class.java)
+    }
+}
