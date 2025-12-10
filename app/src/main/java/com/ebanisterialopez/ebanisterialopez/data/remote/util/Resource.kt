8fe3c789910ba3cc87c918980603dd4d167dde88
malvin <Malvinjose16@gmail.com>
2025-12-09 20:32:42 -0400

    Crea Resourse

--- app/src/main/java/com/ebanisterialopez/ebanisterialopez/data/remote/util/Resource.kt
+++ app/src/main/java/com/ebanisterialopez/ebanisterialopez/data/remote/util/Resource.kt
@@ -1,6 +1,4 @@
-package com.ebanisterialopez.ebanisterialopez.data.api.util
-
-
+package com.ebanisterialopez.ebanisterialopez.data.remote.util
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
