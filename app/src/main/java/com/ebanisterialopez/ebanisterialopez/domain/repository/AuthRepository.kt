aab1ff0e41e9d3cbd6a6e60d2650c8da8b44181d
malvin <Malvinjose16@gmail.com>
2025-12-09 20:18:12 -0400

    Modifica AuthRepository.kt

--- app/src/main/java/com/ebanisterialopez/ebanisterialopez/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/ebanisterialopez/ebanisterialopez/domain/repository/AuthRepository.kt
@@ -1,13 +1,7 @@
 package com.ebanisterialopez.ebanisterialopez.domain.repository
 
-
-import com.ebanisterialopez.ebanisterialopez.data.api.util.Resource
-import com.ebanisterialopez.ebanisterialopez.domain.model.AuthModel
-import kotlinx.coroutines.flow.Flow
-
+import com.ebanisterialopez.ebanisterialopez.domain.model.AuthToken
 interface AuthRepository {
-    suspend fun login(userName: String, password: String): Resource<AuthModel>
-    suspend fun createUser(userName: String, password: String, name: String): Resource<AuthModel>
-    fun isAuthenticated(): Flow<Boolean>
-    suspend fun logout()
+    suspend fun login(email: String, password: String): AuthToken
+    suspend fun register(email: String, password: String): AuthToken
 }
