7d20794c80b4d4a7aceb96f16695fcfa5e3ff357
malvin <Malvinjose16@gmail.com>
2025-12-09 20:17:01 -0400

    Crea AppModule.kt para conectar con la api

--- app/src/main/java/com/ebanisterialopez/ebanisterialopez/di/AppModule.kt
+++ app/src/main/java/com/ebanisterialopez/ebanisterialopez/di/AppModule.kt
@@ -1,33 +1,73 @@
 package com.ebanisterialopez.ebanisterialopez.di
 
-
-import com.ebanisterialopez.ebanisterialopez.data.api.AuthApiService
+import com.ebanisterialopez.ebanisterialopez.data.remote.dto.AuthApi
+import com.ebanisterialopez.ebanisterialopez.data.remote.dto.ProductApi
+import com.ebanisterialopez.ebanisterialopez.data.remote.dto.SalesApi
+import com.ebanisterialopez.ebanisterialopez.data.remote.repository.SalesRepositoryImpl
+import com.ebanisterialopez.ebanisterialopez.domain.repository.SalesRepository
+import com.ebanisterialopez.ebanisterialopez.domain.usecase.CrearVentaUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
-
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/"
-
+    private const val BASE_URL = "https://ebanisterialopez-api-gkech8c3f5f2f0eq.centralus-01.azurewebsites.net/"
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideAuthApi(client: OkHttpClient): AuthApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(AuthApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideProductApi(client: OkHttpClient): ProductApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideSalesApi(client: OkHttpClient): SalesApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SalesApi::class.java)
+    }
+    @Provides
+    @Singleton
+    fun provideSalesRepository(api: SalesApi): SalesRepository {
+        return SalesRepositoryImpl(api)
     }
 
     @Provides
     @Singleton
-    fun provideAuthApiService(retrofit: Retrofit): AuthApiService {
-        return retrofit.create(AuthApiService::class.java)
+    fun provideCrearVentaUseCase(repository: SalesRepository): CrearVentaUseCase {
+        return CrearVentaUseCase(repository)
     }
 }
