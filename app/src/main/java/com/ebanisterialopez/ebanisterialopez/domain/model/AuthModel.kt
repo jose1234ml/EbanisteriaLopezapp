ce1bd67f9546ca30bcd19dd2a6d7326fb411893b
malvin <Malvinjose16@gmail.com>
2025-12-09 20:20:14 -0400

    Modifica AuthToken.kt  funcionar con la api de mi pagina wed

--- app/src/main/java/com/ebanisterialopez/ebanisterialopez/domain/model/AuthModel.kt
+++
@@ -1,8 +0,0 @@
-package com.ebanisterialopez.ebanisterialopez.domain.model
-
-
-data class AuthModel(
-    val userId: Int?,
-    val userName: String?,
-    val token: String?
-)
---
+++ app/src/main/java/com/ebanisterialopez/ebanisterialopez/domain/model/AuthToken.kt
@@ -0,0 +1,5 @@
+package com.ebanisterialopez.ebanisterialopez.domain.model
+data class AuthToken(
+    val token: String,
+    val userId: Int
+)
